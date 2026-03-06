10cb93f2b40b28035718d7c3a0222473392e9f30
Bucket-Daddy <rasmustheilgaardandersen@gmail.com>
2026-03-06 15:17:49 +0100

    Update Slots frontend script.py to version 0.4

--- Slots frontend script.py
+++ Slots frontend script.py
@@ -7,14 +7,15 @@ import math
 pygame.init()
 
 #Definerer grafik variabler
-symbolScale = 2
-symbolSpaceVer = 16
-spinSpeed = 10
-symbolSpaceHor = 100
-squareDist = 5
+overallScale = 2
+symbolScale = 2 * overallScale
+symbolSpaceVer = 16 * overallScale
+spinSpeed = 10 * overallScale
+symbolSpaceHor = 100 * overallScale
+squareDist = 5 * overallScale
 frameRate = 60
-font = pygame.font.Font(None, size = 30)
-dividerLineWidth = 8
+font = pygame.font.Font(None, size = 30 * overallScale)
+dividerLineWidth = 8 * overallScale
 
 #definerer reel variabler
 reels = []
@@ -38,6 +39,13 @@ diamond = pygame.image.load('assets/SymbolDiamond.webp')
 treasure = pygame.image.load('assets/SymbolTreasureChest.webp')
 seven = pygame.image.load('assets/SymbolSeven.webp')
 
+golden = pygame.image.load('assets/modifierGolden.webp')
+token = pygame.image.load('assets/modifierToken.webp')
+ticket = pygame.image.load('assets/modifierTicket.webp')
+repetition = pygame.image.load('assets/modifierRepetition.webp')
+battery = pygame.image.load('assets/modifierBattery.webp')
+chain = pygame.image.load('assets/modifierChain.webp')
+
 #Skalerer billeder
 lemon = pygame.transform.scale(lemon, (18 * symbolScale, 18 * symbolScale))
 cherry = pygame.transform.scale(cherry, (18 * symbolScale, 18 * symbolScale))
@@ -47,11 +55,21 @@ diamond = pygame.transform.scale(diamond, (18 * symbolScale, 18 * symbolScale))
 treasure = pygame.transform.scale(treasure, (18 * symbolScale, 18 * symbolScale))
 seven = pygame.transform.scale(seven, (18 * symbolScale, 18 * symbolScale))
 
+golden = pygame.transform.scale(golden, (9 * symbolScale, 9 * symbolScale))
+token = pygame.transform.scale(token, (9 * symbolScale, 9 * symbolScale))
+ticket = pygame.transform.scale(ticket, (9 * symbolScale, 9 * symbolScale))
+repetition = pygame.transform.scale(repetition, (9 * symbolScale, 9 * symbolScale))
+battery = pygame.transform.scale(battery, (9 * symbolScale, 9 * symbolScale))
+chain = pygame.transform.scale(chain, (9 * symbolScale, 9 * symbolScale))
+
 #temporary simulation of result from slots script
 res = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
-result = [('vert1', 2, ()), ('vert3', 2, ()), ('vert5', 2, ()), ('bckDiag2', 2, ()), ('fwdDiag2', 2, ()), ('horXL2', 6, ()), ('above', 14, ()), ('below', 14, ()), ('eye', 16, ()), ('jackpot', 20, ())]
+result = [('vert1', 2190890, ()), ('vert3', 2, ()), ('vert5', 2, ()), ('bckDiag2', 2, ()), ('fwdDiag2', 2, ()), ('horXL2', 6, ()), ('above', 14, ()), ('below', 14, ()), ('eye', 16, ()), ('jackpot', 20, ())]
+modifiers = [0, 0, 0, 0, 0, 1, 5, 0, 0, 0, 0, 0, 1, 3, 5]
 
-symbols = {0:lemon, 1:cherry, 2:clover, 3:bell, 4:diamond, 5:treasure, 6:seven}
+#Tuples og dictionaries til fortolkning af resultat
+symbolsTuple = (lemon, cherry, clover, bell, diamond, treasure, seven)
+modifiersTuple = (None, golden, token, ticket, repetition, battery, chain)
 patterns = {'hor1.1':(0, 1, 2), 'hor1.2':(1, 2, 3), 'hor1.3':(2, 3, 4), 'hor2.1':(5, 6, 7), 'hor2.2':(6, 7, 8), 'hor2.3':(7, 8, 9),
             'hor3.1':(10, 11, 12), 'hor3.2':(11, 12, 23), 'hor3.3':(12, 13, 14), 'vert1':(0, 5, 10), 'vert2':(1, 6, 11), 'vert3':(2, 7, 12),
             'vert4':(3, 8, 13), 'vert5':(4, 9, 14), 'bckDiag1':(0, 6, 12), 'bckDiag2':(1, 7, 13), 'bckDiag3':(2, 8, 14), 
@@ -67,7 +85,9 @@ for i in range(5):
     reel = pygame.Surface((18 * symbolScale, (18 * symbolScale + symbolSpaceVer) * 30), pygame.SRCALPHA)
 
     for slot in range(3):
-        reel.blit(symbols[res[slot * 5 + i]], (0, slot * (18 * symbolScale + symbolSpaceVer)))
+        reel.blit(symbolsTuple[res[slot * 5 + i]], (0, slot * (18 * symbolScale + symbolSpaceVer)))
+        if modifiers[slot * 5 + i] > 0:
+            reel.blit(modifiersTuple[modifiers[slot * 5 + i]], (symbolScale * 9, slot * (18 * symbolScale + symbolSpaceVer) + 9 * symbolScale))
 
     for slot in range(27):
         reel.blit(random.choice((lemon, cherry, clover, bell, diamond, treasure, seven)), (0, (18 * symbolScale + symbolSpaceVer) * 3 + slot * (18 * symbolScale + symbolSpaceVer)))
@@ -96,20 +116,25 @@ while running:
     for reel in range(5):
         slotMachine.blit(reels[reel], (symbolSpaceHor - 18 * symbolScale + reel * symbolSpaceHor, reelsY[reel]))
 
+#Tegner linjer mellem reels
+    for i in range(4):
+        pygame.draw.rect(slotMachine, (120, 95, 26), pygame.Rect(1.5 * (symbolSpaceHor - 18 * symbolScale) + i * symbolSpaceHor + 18 * symbolScale - 0.5 * dividerLineWidth, 0, dividerLineWidth, slotMachine.get_height()))
+
 #Når reelsene har noget bunden af skærmen vises hvert pattern for patternDuration sekunder
-    if reelsY.count(16) == 5 and patternTimer < len(result) * frameRate * patternDuration:
+    if reelsY.count(symbolSpaceVer) == 5 and patternTimer < len(result) * frameRate * patternDuration:
 #Tegner kasser om hvert symbol i et givent pattern
         for slot in patterns[result[math.floor(patternTimer / (frameRate * patternDuration))][0]]:
             pygame.draw.rect(slotMachine, (36, 252, 3), pygame.Rect(symbolSpaceHor - 18 * symbolScale - squareDist + slot % 5 * symbolSpaceHor, symbolSpaceVer - squareDist + math.floor(slot / 5) * (18 * symbolScale + symbolSpaceVer), 18 * symbolScale + 2 * squareDist, 18 * symbolScale + 2 * squareDist), 2, 3)
-#Viser værdien af et givent pattern 
-        text = font.render('+' + str(result[math.floor(patternTimer / (frameRate * patternDuration))][1]), True, (245, 179, 66))
+#Viser værdien af et givent pattern
+        if len(str(result[math.floor(patternTimer / (frameRate * patternDuration))][1])) > 7:
+            patternPayout = round(result[math.floor(patternTimer / (frameRate * patternDuration))][1] * 10**(-1 * (len(str(result[math.floor(patternTimer / (frameRate * patternDuration))][1])) - 1)), 5)
+            text = font.render('+' + str(patternPayout) + 'E+' + str(len(str(result[math.floor(patternTimer / (frameRate * patternDuration))][1])) - 1), True, (250, 10, 10))
+        else:
+            text = font.render('+' + str(result[math.floor(patternTimer / (frameRate * patternDuration))][1]), True, (246, 250, 10))
+
         slotMachine.blit(text , text.get_rect(center=(slotMachine.get_width()/2, slotMachine.get_height()/2)))
 
         patternTimer += 1
-
-#Tegner linjer mellem reels
-    for i in range(4):
-        pygame.draw.rect(slotMachine, (237, 156, 50), pygame.Rect(1.5 * (symbolSpaceHor - 18 * symbolScale) + i * symbolSpaceHor + 18 * symbolScale - 0.5 * dividerLineWidth, 0, dividerLineWidth, slotMachine.get_height()))
 
 #Lukker spillet når vinduet lukkes
     for event in pygame.event.get():

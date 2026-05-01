1047e119710011f3e8e2b0c42845c1e78c853385
RNGsus <jonathan.holt.lajer@gmail.com>
2026-05-01 13:51:22 +0200

    Changed it so it showes up as long as the proper img are there

--- Cloverpit Project withrooms/frontend/shop_room.py
+++ Cloverpit Project withrooms/frontend/shop_room.py
@@ -9,17 +9,30 @@ class ShopRoom:
         overallScale = 2
         self.font = pygame.font.Font(None, size = 15 * overallScale)
 
-        #Opretter shop surface og danner baggrunden
-        self.shop = pygame.Surface((1200, 750), pygame.SRCALPHA)
+        # danner baggrunden
+        self.shop_room = pygame.Surface((1200, 700), pygame.SRCALPHA)
         background = pygame.image.load('assets/Background.png')
-        background = pygame.transform.scale(background, (1200, 750))
-        self.shop.blit(background, (-5, -5))
+        background = pygame.transform.scale(background, (self.shop_room.get_width(), self.shop_room.get_height()))
+        self.shop_room.blit(background, (-5, -5))
+        self. shop_room.blit(storeSign, (220, 30))
 
-        #Placeholder tekst
-        textShop = self.font.render('--- SHOP ---', True, (246, 250, 10))
-        textComingSoon = self.font.render('coming soon', True, (246, 250, 10))
-        self.shop.blit(textShop, textShop.get_rect(center=(600, 355)))
-        self.shop.blit(textComingSoon, textComingSoon.get_rect(center=(600, 385)))
+        #Loader billeder
+        storePedestals = pygame.image.load('assets/StorePedestals.png')
+        storeSign = pygame.image.load('assets/StoreSign.png')
+        glass = pygame.image.load('assets/Glass_panes.png.png')
+
+        #Skalerer billeder
+        storePedestals = pygame.transform.scale(storePedestals, (storePedestals.get_width(), storePedestals.get_height()))
+        storeSign = pygame.transform.scale(storeSign, (storeSign.get_width() * 0.5, storeSign.get_height() * 0.5))
+        glass = pygame.transform.scale(glass, (glass.get_width(), glass.get_height()))
+
+        # Holdere til items i shop
+        self.shop_room.blit(storePedestals, (350, 170))
+
+        # Selve items i shop
+
+        # glas oven på holderne
+        self.shop_room.blit(glass, (405, 332))
 
     def draw(self, screen):
-        screen.blit(self.shop, (0, 0))
+        screen.blit(self.shop_room, (0, 0))

75e18f5aaccefb508139128ceef3582b8c0acd03
Bucket-Daddy <rasmustheilgaardandersen@gmail.com>
2026-05-01 15:34:48 +0200

    Update shop restocking

--- Cloverpit Project withrooms/backend/shop_restock.py
+++ Cloverpit Project withrooms/backend/shop_restock.py
@@ -1,11 +1,10 @@
 #shop restock
 import random
-shopItems = []
+shopItems = [0, 0, 0, 0]
 
 def shopRestock(unlockedItems, itemWeights):
     shopItemClasses = random.choices(unlockedItems, weights = itemWeights, k = 4)
 
     for item in range(4):
+        global shopItems
         shopItems[item] = shopItemClasses[item]()
-        
-    return shopItems

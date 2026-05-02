cdf270758b9209baf654ece44451352a7ce39304
RNGsus <jonathan.holt.lajer@gmail.com>
2026-05-02 14:46:08 +0200

    Updated locations of (see discription) in shopRoom
    All 4 glass panes
    All 4 Pedestals
    The refresh pedestal
    All 5 individual price tags (you mentioned wanting this, Rasmus)
    Made it so it moves with the size of the screen

--- Cloverpit Project withrooms/frontend/shop_room.py
+++ Cloverpit Project withrooms/frontend/shop_room.py
@@ -10,10 +10,10 @@ class ShopRoom:
         self.font = pygame.font.Font(None, size = 15 * overallScale)
 
         # danner baggrunden
-        self.shop_room = pygame.Surface((1200, 700), pygame.SRCALPHA)
+        self.shop_room = pygame.Surface((1200, 750), pygame.SRCALPHA)
         background = pygame.image.load('assets/Background.png')
-        background = pygame.transform.scale(background, (self.shop_room.get_width(), self.shop_room.get_height()))
-        self.shop_room.blit(background, (-5, -5))
+        background = pygame.transform.scale(background, (1200, 750))
+        self.shop_room.blit(background, (-3, -3))
 
         #Loader billeder
         storePedestal = pygame.image.load('assets/StorePedestal.png')
@@ -36,14 +36,26 @@ class ShopRoom:
         self.shop_room.blit(storePedestal, (self.shop_room.get_width() // 2 - storePedestal.get_width() // 2, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 2))
         self.shop_room.blit(priceTag, (self.shop_room.get_width() // 2 - storePedestal.get_width() // 2 + 2.8, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 8 - 2.8))
 
+        self.shop_room.blit(storePedestal, (self.shop_room.get_width() // 2 + storePedestal.get_width() // 2, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 2))
+        self.shop_room.blit(priceTag, (self.shop_room.get_width() // 2 + storePedestal.get_width() // 2 + 2.8, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 8 - 2.8))
+        
+        self.shop_room.blit(storePedestal, (self.shop_room.get_width() // 2 - storePedestal.get_width() - storePedestal.get_width() // 2, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 2))
+        self.shop_room.blit(priceTag, (self.shop_room.get_width() // 2 - storePedestal.get_width() - storePedestal.get_width() // 2 + 2.8, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 8 - 2.8))
+        
+        self.shop_room.blit(storePedestal, (self.shop_room.get_width() // 2 - storePedestal.get_width() - storePedestal.get_width() - storePedestal.get_width() // 2, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 2))
+        self.shop_room.blit(priceTag, (self.shop_room.get_width() // 2 - storePedestal.get_width() - storePedestal.get_width() - storePedestal.get_width() // 2 + 2.8, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 8 - 2.8))
+
         # refresh standeren
-        #self.shop_room.blit(refresh, (500, 270))
-        #self.shop_room.blit(priceTag, (503, 375))
+        self.shop_room.blit(refresh, (self.shop_room.get_width() // 2 + storePedestal.get_width() + storePedestal.get_width() // 2, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 2))
+        self.shop_room.blit(priceTag, (self.shop_room.get_width() // 2 + storePedestal.get_width() + storePedestal.get_width() // 2 + 2.8, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 8 - 2.8))
 
         # Selve items i shop
 
         # glas oven på holderne
-        self.shop_room.blit(glass, (405, 332))
+        self.shop_room.blit(glass, (self.shop_room.get_width() // 2 - storePedestal.get_width() // 2, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 2 + 2.8))
+        self.shop_room.blit(glass, (self.shop_room.get_width() // 2 + storePedestal.get_width() // 2, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 2 + 2.8))
+        self.shop_room.blit(glass, (self.shop_room.get_width() // 2 - storePedestal.get_width() - storePedestal.get_width() // 2, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 2 + 2.8))
+        self.shop_room.blit(glass, (self.shop_room.get_width() // 2 - storePedestal.get_width() - storePedestal.get_width() - storePedestal.get_width() // 2, self.shop_room.get_height() // 1.7 - storePedestal.get_height() // 2 + 2.8))
 
     def draw(self, screen):
         screen.blit(self.shop_room, (0, 0))

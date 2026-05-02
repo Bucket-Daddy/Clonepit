97b6dba9e2f15c1558917acd392e0e6b82399c73
RNGsus <jonathan.holt.lajer@gmail.com>
2026-05-02 10:41:30 +0200

    Updated placement and highlighting of the boxes
    Also added 3 more boxes and put the text in the correct spot

--- Cloverpit Project withrooms/frontend/phone_room.py
+++ Cloverpit Project withrooms/frontend/phone_room.py
@@ -15,11 +15,11 @@ class PhoneRoom:
         background = pygame.transform.scale(background, (1200, 750))
         self.phone_room.blit(background, (-3, -3))
 
-        #Loader billeder
+        # Loader billeder
         phoneResting = pygame.image.load('assets/Phone (resting).png')
         phoneActive = pygame.image.load('assets/Phone (active).png')
 
-        #Skalerer billeder
+        # Skalerer billeder
         phoneResting = pygame.transform.scale(phoneResting, (phoneResting.get_width() * 0.8, phoneResting.get_height() * 0.8))
         phoneActive = pygame.transform.scale(phoneActive, (phoneActive.get_width() * 1.2, phoneActive.get_height() * 1.2))
         
@@ -27,21 +27,32 @@ class PhoneRoom:
         phoneInteract = pygame.image.load('assets/Phone (interraction).png')
         phoneInteract = pygame.transform.scale(phoneInteract, (phoneInteract.get_width() * 0.5, phoneInteract.get_height() * 0.5))
         
-        # Holdere til items i shop
-        self.phone_room.blit(phoneResting, (self.phone_room.get_width() // 6 - phoneResting.get_width() // 2, 200))
+        # Telefoner
+        # self.phone_room.blit(phoneResting, (self.phone_room.get_width() // 6 - phoneResting.get_width() // 2, 200))
         self.phone_room.blit(phoneActive, (self.phone_room.get_width() // 3.2 - phoneActive.get_width() // 2, 30))
 
         # telefonens teksbokse
-        pygame.draw.rect(self.phone_room, (0, 0, 0), (self.phone_room.get_width() // 3.2 + phoneActive.get_width() // 1.75, 50, self.phone_room.get_width() // 2.5, 250), border_radius=0)
+        pygame.draw.rect(self.phone_room, (0, 0, 0), (self.phone_room.get_width() // 3.2 + phoneActive.get_width() // 1.75, self.phone_room.get_width() // 50, self.phone_room.get_width() // 2.5, self.phone_room.get_width() // 4), 0)
+        pygame.draw.rect(self.phone_room, (232, 132, 44), (self.phone_room.get_width() // 3.2 + phoneActive.get_width() // 1.75, self.phone_room.get_width() // 50, self.phone_room.get_width() // 2.5, self.phone_room.get_width() // 4), 5)
         
+        # valgboks 1, 2 og 3
+        pygame.draw.rect(self.phone_room, (0, 0, 0), (self.phone_room.get_width() // 25, self.phone_room.get_width() // 2.5, self.phone_room.get_width() // 3.4, self.phone_room.get_width() // 8), 0)
+        pygame.draw.rect(self.phone_room, (246, 250, 10), (self.phone_room.get_width() // 25, self.phone_room.get_width() // 2.5, self.phone_room.get_width() // 3.4, self.phone_room.get_width() // 8), 4)
+        
+        pygame.draw.rect(self.phone_room, (0, 0, 0), (self.phone_room.get_width() // 25 + self.phone_room.get_width() // 3.25, self.phone_room.get_width() // 2.5, self.phone_room.get_width() // 3.4, self.phone_room.get_width() // 8), 0)
+        pygame.draw.rect(self.phone_room, (246, 250, 10), (self.phone_room.get_width() // 25 + self.phone_room.get_width() // 3.25, self.phone_room.get_width() // 2.5, self.phone_room.get_width() // 3.4, self.phone_room.get_width() // 8), 4)
+        
+        pygame.draw.rect(self.phone_room, (0, 0, 0), (self.phone_room.get_width() // 25 + self.phone_room.get_width() // 3.25 + self.phone_room.get_width() // 3.25, self.phone_room.get_width() // 2.5, self.phone_room.get_width() // 3.4, self.phone_room.get_width() // 8), 0)
+        pygame.draw.rect(self.phone_room, (246, 250, 10), (self.phone_room.get_width() // 25 + self.phone_room.get_width() // 3.25 + self.phone_room.get_width() // 3.25, self.phone_room.get_width() // 2.5, self.phone_room.get_width() // 3.4, self.phone_room.get_width() // 8), 4)
+
         # telefonens tekst
         textPhone = self.font.render('--- UNKNOWN NUMBER ---', True, (232, 132, 44))
-        textComingSoon = self.font.render('randomized message here', True, (232, 132, 44))
-        textAnswerOne = self.font.render('Answer 1', True, (232, 132, 44))
-        textAnswerTwo = self.font.render('Answer 2', True, (232, 132, 44))
-        textAnswerThree = self.font.render('Answer 3', True, (232, 132, 44))
-        self.phone_room.blit(textPhone, textPhone.get_rect(center=(self.phone_room.get_width() // 2, 355)))
-        self.phone_room.blit(textComingSoon, textComingSoon.get_rect(center=(self.phone_room.get_width() // 2, 385)))        
+        textRandomMessage = self.font.render('randomized message here', True, (232, 132, 44))
+        textAnswerOne = self.font.render('Answer 1', True, (246, 250, 10))
+        textAnswerTwo = self.font.render('Answer 2', True, (246, 250, 10))
+        textAnswerThree = self.font.render('Answer 3', True, (246, 250, 10))
+        self.phone_room.blit(textPhone, textPhone.get_rect(center=(self.phone_room.get_width() // 3.2 + phoneActive.get_width() // 1.75 + self.phone_room.get_width() // 5, self.phone_room.get_width() // 25)))
+        self.phone_room.blit(textRandomMessage, textRandomMessage.get_rect(center=(self.phone_room.get_width() // 3.2 + phoneActive.get_width() // 1.75 + self.phone_room.get_width() // 5, self.phone_room.get_width() // 15)))        
 
         
         

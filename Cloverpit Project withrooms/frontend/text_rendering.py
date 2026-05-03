00ad29a76e699b40f03d3a8966027b74694e467c
Bucket-Daddy <rasmustheilgaardandersen@gmail.com>
2026-05-03 17:59:25 +0200

    Better but more expensive

--- Cloverpit Project withrooms/frontend/text_rendering.py
+++ Cloverpit Project withrooms/frontend/text_rendering.py
@@ -15,24 +15,22 @@ def renderText(text, font, surface, origin, width):
     while len(text) > 1:
         i = 1
         x = origin[0]
-        while font.size(text[:i])[0] < width and i < len(text):
+        while font.size(text[:i].replace('\033[93m', '').replace('\033[92m', '').replace('\033[32m', '').replace('\033[33m', ''))[0] < width and i < len(text):
             i += 1
                 
         if i < len(text):
             i = text.rfind(' ', 0, i) + 1
 
         frags = text[:i].split('\x1b')
-        for j in range(len(frags)):
-            if frags[j].startswith('['):
-                frag = frags[j].split('m', 1)
-                frags[j] = font.render(frag[1], True, colorCodes[frag[0]], (0, 0, 0))
+        for frag in frags:
+            if frag.startswith('['):
+                frag = frag.split('m', 1)
+                frag = font.render(frag[1], True, colorCodes[frag[0]], (0, 0, 0))
             else:
-                frags[j] = font.render(frags[j], True, (255, 255, 255), (0, 0, 0))
+                frag = font.render(frag, True, (255, 255, 255), (0, 0, 0))
             
-            surface.blit(frags[j], (x, y))
-            x += frags[j].get_width()
+            surface.blit(frag, (x, y))
+            x += frag.get_width()
     
-
-
         y += 24
         text = text[i:]

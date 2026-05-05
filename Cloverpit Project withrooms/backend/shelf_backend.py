b032a61c9534cc1314a8635910f7e3107c53deef
Bucket-Daddy <rasmustheilgaardandersen@gmail.com>
2026-05-05 20:32:04 +0200

    Tweaked some imports

--- Cloverpit Project withrooms/backend/shelf_backend.py
+++ Cloverpit Project withrooms/backend/shelf_backend.py
@@ -1,48 +1,48 @@
 #administrering af shelf items
 import random
-import config.game_config as game_config
+import config.game_config as config
 
 def passiveTrigger(item):
     if item.type == 'passive':
         item.trigger()
 
 def lastSpinTrigger():
-    for item in game_config.shelfItems:
+    for item in config.shelfItems:
         if item.type == 'passive':
             item.trigger()
 
 def preRollTrigger():
-    for item in game_config.shelfItems:
+    for item in config.shelfItems:
         if item.type == 'preRoll':
             item.trigger()
 
 def postRollTrigger(pityCounter, result):
-    for item in game_config.shelfItems:
+    for item in config.shelfItems:
         if item.type == 'postRoll':
             item.trigger(pityCounter, result)
 
 def randomTrigger(pityCounter, result):
-    for item in game_config.shelfItems:
-        if item.type == 'random' and 100 * random.random() <= item.chance * game_config.randomTriggerMult:
+    for item in config.shelfItems:
+        if item.type == 'random' and 100 * random.random() <= item.chance * config.randomTriggerMult:
             item.trigger(pityCounter, result)
 
 def buttonTrigger():
-    for item in game_config.shelfItems:
+    for item in config.shelfItems:
         if item.type == 'button' and item.charges > 0:
             item.trigger()
             item.charges -= 1
 
 def roundEndTrigger():
-    for item in game_config.shelfItems:
+    for item in config.shelfItems:
         if item.type == 'roundEnd':
             item.trigger()
 
 def deadlineEndTrigger():
-    for item in game_config.shelfItems:
+    for item in config.shelfItems:
         if item.type == 'deadlineEnd':
             item.trigger()
 
 def interventionTrigger():
-    for item in game_config.shelfItems:
+    for item in config.shelfItems:
         if item.type == 'intervention':
             item.trigger()

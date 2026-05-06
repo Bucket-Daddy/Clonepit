ce9ed9b30159d0bce44e05de068ace1aea729e82
Bucket-Daddy <rasmustheilgaardandersen@gmail.com>
2026-05-06 16:26:41 +0200

    Completely reworked call classes to be more like item classes

--- Cloverpit Project withrooms/backend/call_classes.py
+++ Cloverpit Project withrooms/backend/call_classes.py
@@ -1,238 +1,223 @@
-# Telefon kræfter
-
-
-# i need a thing where it stops triggerring the effect, kinda like lostBriefcase from item_classes.
+# Telefon abilities
 
 import math
-import frontend.text_rendering
 
-class phoneAbility:
-    maxPicks = math.inf
-    pass
+import config.game_config as config
+import frontend.shop_room as shop
 
-# COMMON appears the most. arbitraty number is 100
-# UNCOMMON appears 15% less often than COMMON. arbitraty number is 85
-# RARE appears 35% less often than COMMON. arbitraty number is 65
-# ULTRA RARE appears 60% less often than COMMON. arbitraty number is 40
-# LEGENDARY appears 75% less often than COMMON. arbitraty number is 25
+config.unlockedCalls = []
+config.callWeights = []
 
-# Sum of all the arbitrary numbers is 315
-# chance of getting a COMMON is 100/315
-# UNCOMMON is 85/315, RARE is 65/315
-# ULTRA RARE is 40/315 and LEGENDARY is 25/315
+def callInit():
 
-# COMMON is approximately 31.75%
-# UNCOMMON is approximately 26.98%
-# RARE is approximately 20.63%
-# ULTRA RARE is approximately 12.70%
-# LEGENDARY is approximately 7.94%
+    class itemSpaceCall():
+        Name = 'Please don\'t throw my stuff away!'
+        description = 'Increase Charm\'s space by 1, permanently.'
+        weight = 85
 
-class common (phoneAbility):
-    rarity = 'COMMON'
-    pass
+        def trigger():
+            config.shelfSpace += 1
+            config.unlockedCalls.pop(0)
+            config.callWeights.pop(0)
 
-class uncommon (phoneAbility):
-    rarity = 'UNCOMMON'
-    pass
+    config.unlockedCalls.append(itemSpaceCall)
+    config.callWeights.append(itemSpaceCall.weight)
 
-class rare (phoneAbility):
-    rarity = 'RARE'
-    pass
 
-class ultraRare (phoneAbility):
-    rarity = 'ULTRA RARE'
-    pass
+    class jackpotValueCall():
+        name = 'I can\'t quit now!'
+        description = 'Double Jackpot\'s value'
+        weight = 100
 
-class legendary (phoneAbility):
-    rarity = 'LEGENDARY'
-    pass
+        def trigger():
+            config.patternValues['jackpot'] *= 2
 
-class Please_dont_throw_my_stuff_away (uncommon):
-    # Increase Charm's space by 1, permanently.
-    maxPicks = 1
+    config.unlockedCalls.append(jackpotValueCall)
+    config.callWeights.append(jackpotValueCall.weight)
 
-    def trigger(self, origin, width):
-        callText = 'Please don\'t throw my stuff away!'
-        callDiscription = '\033[93mIncrease Charm\'s space by 1, permanently\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    class ticketCall():
+        name = 'Can I borrow some green?'
+        description = '+5 Clover Tickets'
+        weight = 100
 
-class I_cant_quit_now (common):
-    # Double Jackpot's value.
+        def trigger():
+            config.tickets += 5
 
-    def trigger(self, origin, width):
-        callText = 'I can\'t quit now!'
-        callDiscription = '\033[93mDouble Jackpot\'s value\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    config.unlockedCalls.append(ticketCall)
+    config.callWeights.append(ticketCall.weight)    
 
-class Can_I_borrow_some_green (common):
-    # +5 tickets
-
-    def trigger(self, origin, width):
-        callText = 'Can I borrow some green?'
-        callDiscription = '\033[93m+5 tickets\033[0m'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
-
-class Can_I_eat_something (common):
-    # Lucky Charms at the store are discounted by 2 until the next Restock of the store.
+    class shopDiscountCall():
+        name = 'Can I eat something?'
+        description = 'Lucky Charms at the store are discounted by 2 until the next Restock of the store.'
+        weight = 100
     
-    # this def needs something to disable the effect after the next restock
-    def trigger(self, origin, width):
-        callText = 'Can I eat something?'
-        callDiscription = '\033[93mLucky Charms\033[0m at the \033[93mstore\033[0m are \033[93mdiscounted by 2\033[0m until the \033[93mnext Restock\033[0m of the store.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+        def trigger():
+            for item in shop.shopItems:
+                item.cost -= 2
+            
+    config.unlockedCalls.append(shopDiscountCall)
+    config.callWeights.append(shopDiscountCall.weight)
 
-class Can_you_give_me_some_Energy_Drinks (common):
-    # Restore all Charges on all Charms triggered by the Red Button
+    class itemChargeCall():
+        name = 'Can you give me some Energy Drinks?'
+        description = 'Restore all Charges on all Charms triggered by the Red Button.'
+        weight = 100
 
-    def trigger(self, origin, width):
-        callText = 'Can you give me some Energy Drinks?'
-        callDiscription = 'Restore \033[93mall Charges\033[0m on all \033[93mCharms triggered\033[0m by the \033[93mRed Button\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+        def trigger():
+            for item in config.shelfItems:
+                if item.type == 'button' and item.charges < item.chargeSlots:
+                    item.charges = item.chargeSlots
+            
+    config.unlockedCalls.append(itemChargeCall)
+    config.callWeights.append(itemChargeCall.weight)
 
-class lemonManifest (common):
-    # Lemons manifest more often (+1), permanently.
+    class lemonCall():
+        name = 'Life gave me lemons'
+        description = 'Lemons manifest more often (+1), permanently.'
+        weight = 100
 
-    def trigger(self, origin, width):
-        callText = 'Life gave me lemons'
-        callDiscription = '\033[93mLemons manifest\033[0m more often \033[93m(+1)\033[0m, \033[93mpermanently\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+        def trigger():
+            config.symbolWeights[0] += 1
 
-class cherryManifest (common):
-    # Cherries manifest more often (+1), permanently.
+    config.unlockedCalls.append(lemonCall)
+    config.callWeights.append(lemonCall.weight)
 
-    def trigger(self, origin, width):
-        callText = 'I used to eat healthy...'
-        callDiscription = '\033[93mCherries manifest\033[0m more often \033[93m(+1)\033[0m, \033[93mpermanently\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    class cherryCall():
+        name = 'I used to eat healthy...'
+        description = 'Cherries manifest more often (+1), permanently.'
+        weight = 100
 
-class cloverManifest (uncommon):
-    # Clovers manifest more often (+1), permanently.
+        def trigger():
+            config.symbolWeights[1] += 1
 
-    def trigger(self, origin, width):
-        callText = 'Today\'s my Lucky day!'
-        callDiscription = '\033[93mClovers manifest\033[0m more often \033[93m(+1)\033[0m, \033[93mpermanently\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    config.unlockedCalls.append(cherryCall)
+    config.callWeights.append(cherryCall.weight)
 
-class bellManifest (uncommon):
-    # Bells manifest more often (+1), permanently.
+    class cloverCall():
+        name = 'Today\'s my Lucky day!'
+        description = 'Clovers manifest more often (+1), permanently.'
+        weight = 85
 
-    def trigger(self, origin, width):
-        callText = 'I need to be there!'
-        callDiscription = '\033[93mBells manifest\033[0m more often \033[93m(+1)\033[0m, \033[93mpermanently\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+        def trigger():
+            config.symbolWeights[2] += 1
 
-class diamondManifest (rare):
-    # Diamonds manifest more often (+1), permanently.
+    config.unlockedCalls.append(cloverCall)
+    config.callWeights.append(cloverCall.weight)
 
-    def trigger(self, origin, width):
-        callText = 'Please! I\'ll give you anything!'
-        callDiscription = '\033[93mDiamonds manifest\033[0m more often \033[93m(+1)\033[0m, \033[93mpermanently\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    class bellCall():
+        name = 'I need to be there!'
+        description = 'Bells manifest more often (+1), permanently.'
+        weight = 85
 
-class treasureManifest (rare):
-    # Treasures manifest more often (+1), permanently.
+        def trigger():
+            config.symbolWeights[3] += 1
 
-    def trigger(self, origin, width):
-        callText = 'I need money!'
-        callDiscription = '\033[93mTreasures manifest\033[0m more often \033[93m(+1)\033[0m, \033[93mpermanently\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    config.unlockedCalls.append(bellCall)
+    config.callWeights.append(bellCall.weight)
 
-class sevenManifest (ultraRare):
-    # Sevens manifest more often (+1), permanently.
+    class diamondCall():
+        name = 'Please! I\'ll give you anything!'
+        description = 'Diamonds manifest more often (+1), permanently.'
+        weight = 65
 
-    def trigger(self, origin, width):
-        callText = 'I didn\'t hurt anybody...'
-        callDiscription = '\033[93mSevens manifest\033[0m more often \033[93m(+1)\033[0m, \033[93mpermanently\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
-    
-class lemonValue (common):
-    # Double the value of the lemon
+        def trigger():
+            config.symbolWeights[4] += 1
 
-    def trigger(self, origin, width):
-        callText = 'I need supplements!'
-        callDiscription = '\033[93mDouble\033[0m the \033[93mvalue\033[0m of the \033[93mlemon\033[0m'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    config.unlockedCalls.append(diamondCall)
+    config.callWeights.append(diamondCall.weight)
 
-class cherryValue (common):
-    # Double the value of the cherry
+    class treasureCall():
+        name = 'I need money!'
+        description = 'Diamonds manifest more often (+1), permanently.'
+        weight = 65
+        def trigger():
+            config.symbolWeights[5] += 1
 
-    def trigger(self, origin, width):
-        callText = 'I need supplements!'
-        callDiscription = '\033[93mDouble\033[0m the \033[93mvalue\033[0m of the \033[93mcherry\033[0m'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    config.unlockedCalls.append(treasureCall)
+    config.callWeights.append(treasureCall.weight)
 
-class cloverValue (common):
-    # Double the value of the clover
+    class sevenCall():
+        name = 'I didn\'t hurt anybody...'
+        description = 'Sevens manifest more often (+1), permanently.'
+        weight = 40
 
-    def trigger(self, origin, width):
-        callText = 'I\'m feeling lucky!'
-        callDiscription = '\033[93mDouble\033[0m the \033[93mvalue\033[0m of the \033[93mclover\033[0m'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+        def trigger():
+            config.symbolWeights[6] += 1
 
-class bellValue (common):
-    # Double the value of the bell
+    config.unlockedCalls.append(sevenCall)
+    config.callWeights.append(sevenCall.weight)
 
-    def trigger(self, origin, width):
-        callText = 'I\'m feeling lucky!'
-        callDiscription = '\033[93mDouble\033[0m the \033[93mvalue\033[0m of the \033[93mbell\033[0m'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    class lemonCherryValue():
+        name = 'I need supplements!'
+        description = 'Double the value of Lemons and Cherries.'
+        weight = 100
 
-class diamondValue (uncommon):
-    # Double the value of the diamond
+        def trigger(self):
+            config.symbolValues[0] *= 2
+            config.symbolValues[1] *= 2
 
-    def trigger(self, origin, width):
-        callText = 'Gold and Diamonds are a good investment!'
-        callDiscription = '\033[93mDouble\033[0m the \033[93mvalue\033[0m of the \033[93mdiamond\033[0m'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    config.unlockedCalls.append(lemonCherryValue)
+    config.callWeights.append(lemonCherryValue.weight)
 
-class treasureValue (uncommon):
-    # Double the value of the treasure
+    class cloverBellValue():
+        name = 'I\'m feeling lucky!'
+        description = 'Double the value of Clovers and Bells.'
+        weight = 100
 
-    def trigger(self, origin, width):
-        callText = 'Gold and Diamonds are a good investment!'
-        callDiscription = '\033[93mDouble\033[0m the \033[93mvalue\033[0m of the \033[93mtreasure\033[0m'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+        def trigger():
+            config.symbolValues[2] *= 2 
+            config.symbolValues[3] *= 2
 
-class sevenValue (uncommon):
-    # Triple the value of the Seven
+    config.unlockedCalls.append(cloverBellValue)
+    config.callWeights.append(cloverBellValue.weight)
 
-    def trigger(self, origin, width):
-        callText = 'I\'m gonna go "\033[93mAll In\033[0m"!'
-        callDiscription = '\033[93mTriple\033[0m the \033[93mvalue\033[0m of the \033[93mSeven\033[0m'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    class diamondTreasureValue():
+        name = 'Gold and Diamonds are a good investment!'
+        description = 'Double the value of Diamonds and Treasures.'
+        weight = 85
 
-class patternValue (uncommon):
-    # Increase the value of Patterns with 3 or less/4+ symbols by their base value.
+        def trigger():
+            config.symbolValues[4] *= 2 
+            config.symbolValues[5] *= 2
 
-    def trigger(self, origin, width):
-        patternValueRandom =round(math.random())
-        
-        if patternValueRandom == 0:
-            callText = 'I\'m thinking of some strategies!'
-            callDiscription = '\033[93mIncrease\033[0m the \033[93mvalue\033[0m of Patterns with \033[93m3 or less symbols\033[0m by their \033[93mbase value\033[0m.'
-        else:
-            callText = 'I found a winning strategy!'
-            callDiscription = '\033[93mIncrease\033[0m the \033[93mvalue\033[0m of Patterns with \033[93m4+ symbols\033[0m by their \033[93mbase value\033[0m.'
-        frontend.text_rendering.renderText(callDiscription, self.font, self.surface, origin, width)
-        pass
+    config.unlockedCalls.append(diamondTreasureValue)
+    config.callWeights.append(diamondTreasureValue.weight)
 
+    class sevenValue():
+        name = 'I\'m gonna go \"All In\"!'
+        description = 'Triple the value of the Seven.'
+        weight = 85
 
+        def trigger():
+            config.symbolValues[6] *= 3
+
+    config.unlockedCalls.append(sevenValue)
+    config.callWeights.append(sevenValue.weight)
+
+    class smallPatternValue():
+        name = 'I\'m thinking of some strategies!'
+        description = 'Increase the value of Patterns with 3 or less symbols by their base value.'
+        weight = 85
+
+        def trigger():
+            config.patternValues['hor'] += config.basePatternValues['hor']
+            config.patternValues['vert'] += config.basePatternValues['vert']
+            config.patternValues['diag'] += config.basePatternValues['diag']
+
+    config.unlockedCalls.append(smallPatternValue)
+    config.callWeights.append(smallPatternValue.weight)
+
+    class bigPatternValue():
+        name = 'I found a winning strategy!'
+        description = 'Increase the value of Patterns with 4+ symbols by their base value.'
+        weight = 85
+
+        def trigger():
+            config.patternValues['horL'] += config.basePatternValues['horL']
+            config.patternValues['horXL'] += config.basePatternValues['horXL']
+            config.patternValues['zig'] += config.basePatternValues['zig']
+            config.patternValues['zag'] += config.basePatternValues['zag']
+            config.patternValues['above'] += config.basePatternValues['above']
+            config.patternValues['below'] += config.basePatternValues['below']
+            config.patternValues['eye'] += config.basePatternValues['eye']
+            config.patternValues['jackpot'] += config.basePatternValues['jackpot']

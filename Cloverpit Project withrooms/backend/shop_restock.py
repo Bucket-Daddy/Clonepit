8117fc9f52adbe532b8306a27a3490c4a291b8fe
Bucket-Daddy <rasmustheilgaardandersen@gmail.com>
2026-05-06 20:04:17 +0200

    Changed shop restock to disallow repeat items

--- Cloverpit Project withrooms/backend/shop_restock.py
+++ Cloverpit Project withrooms/backend/shop_restock.py
@@ -1,9 +1,28 @@
 #shop restock
+import config.game_config as config
 import random
 shopItems = [0, 0, 0, 0]
 
-def shopRestock(unlockedItems, itemWeights):
-    shopItemClasses = random.choices(unlockedItems, weights = itemWeights, k = 4)
+def shopRestock():
+    shopItemClasses = []
+    itemPool = []
+    itemPoolWeights = []
+          
+    for i in range(len(config.unlockedItems)):
+        new = True
+        for item in config.shelfItems:
+            if config.unlockedItems[i].name == item.name:
+                new = False
+
+        if new:
+            itemPool.append(config.unlockedItems[i])
+            itemPoolWeights.append(config.itemWeights[i])
+
+    for i in range(4):
+        item = random.choices(itemPool, weights = itemPoolWeights, k = 1)
+        shopItemClasses.append(item[0])
+        itemPoolWeights.pop(itemPool.index(item[0]))
+        itemPool.remove(item[0])
 
     for item in range(4):
         global shopItems
